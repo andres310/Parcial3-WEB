5c5c9ec7e7b4fddefc0db5393a2d415001084321
Andrés <ia.andresalexander@ufg.edu.sv>
2023-11-04 10:43:38 -0600

    Agregado api deptos y municipios

--- src/main/java/sv/edu/ufg/rrhh/service/DepartamentoService.java
+++ src/main/java/sv/edu/ufg/rrhh/service/DepartamentoService.java
@@ -1,7 +1,11 @@
 package sv.edu.ufg.rrhh.service;
 
 import org.springframework.stereotype.Service;
+import sv.edu.ufg.rrhh.projection.IDeptoProjection;
 import sv.edu.ufg.rrhh.repository.IDepartamentoRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class DepartamentoService {
@@ -12,5 +16,17 @@ public class DepartamentoService {
         this.departamentoRepository = departamentoRepository;
     }
 
+    public List<IDeptoProjection> getAll() {
+        return this.departamentoRepository.findAllView();
+    }
 
+    public IDeptoProjection getOneById(Integer id) {
+        return this.departamentoRepository.getOneById(id)
+                .orElseThrow(() -> new NoSuchElementException(String.format("No se encontro el departamento por su id", id)));
+    }
+
+    public IDeptoProjection getOneByNombre(String nombre) {
+        return this.departamentoRepository.getOneByNombre(nombre)
+                .orElseThrow(() -> new NoSuchElementException(String.format("No se encontro el depto con nombre: ", nombre)));
+    }
 }
